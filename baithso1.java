79e80e01a05eca0fddc1a160e5ca3bdbdf3a1f5b
Datgmer <97456907+Datgmer@users.noreply.github.com>
2022-01-17 17:20:50 +0700

    Add files via upload

---
+++ baithso1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class baithso1 {
+    public static void main(String[] args){
+        Scanner d = new Scanner(System.in);
+        String hoten="";
+        System.out.println("Ho ten: ");
+        hoten= d.nextLine();
+        System.out.println("chao " + hoten);
+    }
+}
